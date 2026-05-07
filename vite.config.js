ecea006e20280ba0cc3fb73d66c93e305d7b68b3
Gulbadin Hasan <gulbadinhasan@Gulbadins-MacBook-Pro.local>
2026-05-07 20:10:57 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
   // ONLY add this if your repo is NOT yourusername.github.io
