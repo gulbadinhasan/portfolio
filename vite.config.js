c125043614efcd639965927db2dfd6adf7392a69
Gulbadin Hasan <gulbadinhasan@Gulbadins-MacBook-Pro.local>
2026-05-07 10:16:38 +0530

    remove blank space

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ONLY add this if your repo is NOT yourusername.github.io
+  // Replace 'devops-portfolio' with your actual repository name
+  base: '/portfolio/',
 })
